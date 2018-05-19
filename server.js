a3722c2132a5f223816552a01231fa98b2246264
Ranjithdss15 <ranjithdss15@gmail.com>
2018-05-19 17:54:04 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+//var morgan = require('morgan');
+//var path = require('path');
+var path = require('path');
+var app = express();
+
+app.set('port',process.env.PORT || 3000);
+
+app.get('/', function (req, res) {
+  
+res.sendFile(path.join(__dirname, 'pages', 'index.html'));
+});
+app.listen(app.get('port'), function(){
+	console.log('Express Started');
+});
