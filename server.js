36796794aab8882517a4e9b29317c4a70dd9d371
EC2 Default User <ec2-user@ip-172-31-17-27.us-east-2.compute.internal>
2018-05-19 13:42:00 +0000

    port changed from 80 to 8080

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.set('port',process.env.PORT || 80 );
+app.set('port',process.env.PORT || 8080 );
 
 app.get('/', function (req, res) {
   
