6e0100f1dc128990d3625aed82a11443273ad46f
Ranjithdss15 <ranjithdss15@gmail.com>
2018-05-23 23:38:57 +0530

    GST Calc complete, changes to alignment.

--- pages/main.js
+++ pages/main.js
@@ -4,13 +4,27 @@ var price = document.getElementById('price');
 var total = document.getElementById('total');
 var calculate = document.getElementById('calculate');
 var taxed = document.getElementById('taxed');
-price.onfocus;
+price.focus	();
 
 
 calculate.onclick = function() {
-	var price1 = price;
 	var gstprice = 18;
-	taxed.innerHTML = gstprice.value;
-total.value = price1.value;
+	var taxed1 = (price.value / 100 ) * 18;
+	var total1 = parseInt(taxed1) + parseInt(price.value) ;
+	taxed.value = taxed1;
+	total.value = total1;
+	
 };
+	document.getElementById('price').onblur = function() {
+		
+	//	window.alert("OnFocus");
+		console.log("OnFocus");
+		document.getElementById('calculate').focus()	;
+		
+	};
+	document.getElementById('calculate').onmouseover = function() {
+		
+		window.confirm("Continue");
+		console.log("Click here to calculate gst")
+	};
 };
