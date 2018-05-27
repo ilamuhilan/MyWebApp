c3cf88a396c51104ff23f9c305e59dc1ffccf19e
Ranjithdss15 <ranjithdss15@gmail.com>
2018-05-27 23:35:44 +0530

    New UI(ProfilePage),but not js added.

--- server.js
+++ server.js
@@ -15,10 +15,19 @@ res.sendFile(path.join(__dirname, 'pages', 'index.html'));
 app.get('/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'pages', 'style.css'));
   });
-  
+  app.get('/styl.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'pages', 'styl.css'));
+  });
   app.get('/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'pages', 'main.js'));
   });
+  app.get('/coverpicCroped.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'Images', 'coverpicCroped.jpg'));
+});
+ app.get('/profilepiccroped.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'Images', 'profilepiccroped.jpg'));
+});
+  
   
   
 app.listen(app.get('port'), function(){
