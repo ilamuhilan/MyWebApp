548fea89cc5aae82039dd7092149471579193a36
Ranjithdss15 <ranjithdss15@gmail.com>
2018-06-05 01:22:21 +0530

    Temp LikeComment,ipPort var added.PostNext fixed.

--- pages/main.js
+++ pages/main.js
@@ -4,6 +4,7 @@ var price = document.getElementById('price');
 var total = document.getElementById('total');
 var calculate = document.getElementById('calculate');
 var taxed = document.getElementById('taxed');
+var ipPort = "localhost:8080"
 price.focus	();
 
 
@@ -27,6 +28,8 @@ calculate.onclick = function() {
 		//window.confirm("Continue");
 		console.log("Click here to calculate gst")
 	};
+
+	//like 1
 	var blikeele  = document.getElementById("likebutton");
 blikeele.onclick = function() {
     //alert('Liked');
@@ -41,9 +44,49 @@ blikeele.onclick = function() {
         } 
         }
     };
-	request.open('GET','http://52.15.135.89:8080/counter',true);
+	request.open('GET','http://'+ipPort+'/counter',true);
         request.send(null);
 };
+
+//like 2
+	var blikeele2  = document.getElementById("likebutton2");
+blikeele2.onclick = function() {
+    //alert('Liked');
+    var request =new XMLHttpRequest();
+    request.onreadystatechange =function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status ===200){
+            var counter2 =  request.responseText;
+            var span=document.getElementById('likecount2');
+            span.innerHTML = counter2.toString();
+           
+        } 
+        }
+    };
+	request.open('GET','http://'+ipPort+'/counter2',true);
+        request.send(null);
+};
+
+//like 3
+	var blikeele3  = document.getElementById("likebutton3");
+blikeele3.onclick = function() {
+    //alert('Liked');
+    var request =new XMLHttpRequest();
+    request.onreadystatechange =function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status ===200){
+            var counter3 =  request.responseText;
+            var span=document.getElementById('likecount3');
+            span.innerHTML = counter3.toString();
+           
+        } 
+        }
+    };
+	request.open('GET','http://'+ipPort+'/counter3',true);
+        request.send(null);
+};
+
+//Comment 1
 var commentsop = document.getElementById('commentoutput');
 var submitclick = document.getElementById('commentbutton');
  submitclick.onclick = function() {
@@ -66,10 +109,80 @@ var commentsip = document.getElementById('commentin');
 							} 
 							}
 						};
-						request.open('GET','http://52.15.135.89:8080/submit-comment?commentinput='+commentsipvalue,true);
+						request.open('GET','http://'+ipPort+'/submit-comment?commentinput='+commentsipvalue,true);
             request.send(null);
 	}
  };
 
+//Comment 2
+var commentsop2 = document.getElementById('commentoutput2');
+var submitclick2 = document.getElementById('commentbutton2');
+ submitclick2.onclick = function() {
+var commentsip2 = document.getElementById('commentin2');
+ commentsipvalue2=commentsip2.value;
+ if(commentsipvalue2!=='')
+    {
+		var request =new XMLHttpRequest();
+        request.onreadystatechange =function() {
+            if(request.readyState === XMLHttpRequest.DONE) {
+                if(request.status ===200){
+                    var list = ' ';
+                var comments2 =  request.responseText;
+				comments2=JSON.parse(comments2);
+				 for(var temp=0;temp<comments2.length;++temp) {
+							 list += ('<li>' + comments2[temp]);
+							  commentsop2.innerHTML = list.toString();
+							  commentsip2.value="";
+							}
+							} 
+							}
+						};
+						request.open('GET','http://'+ipPort+'/submit-comment2?commentinput2='+commentsipvalue2,true);
+            request.send(null);
+	}
+ };
+
+//Comment 3
+var commentsop3 = document.getElementById('commentoutput3');
+var submitclick3 = document.getElementById('commentbutton3');
+ submitclick3.onclick = function() {
+var commentsip3 = document.getElementById('commentin3');
+ commentsipvalue3=commentsip3.value;
+ if(commentsipvalue3!=='')
+    {
+		var request =new XMLHttpRequest();
+        request.onreadystatechange =function() {
+            if(request.readyState === XMLHttpRequest.DONE) {
+                if(request.status ===200){
+                    var list = ' ';
+                var comments3 =  request.responseText;
+				comments3=JSON.parse(comments3);
+				 for(var temp=0;temp<comments3.length;++temp) {
+							 list += ('<li>' + comments3[temp]);
+							  commentsop3.innerHTML = list.toString();
+							  commentsip3.value="";
+							}
+							} 
+							}
+						};
+						request.open('GET','http://'+ipPort+'/submit-comment3?commentinput3='+commentsipvalue3,true);
+            request.send(null);
+	}
+ };
+
+
+
+
+
+
+
+	function myStatus() {
+		
+		var status = document.getElementById("statusid").value;
+			if( status == "Available" )
+				{
+					window.alert("Available");
+				}
+	}
 	
 };
--- server.js
+++ server.js
@@ -27,18 +27,44 @@ app.get('/style.css', function (req, res) {
  app.get('/profilepiccroped.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'Images', 'profilepiccroped.jpg'));
 });
- var counter = 0;
+ var counter = 0; 
 app.get('/counter', function (req, res) {
     counter+=1;
     res.send(counter.toString());
     });
+
+ var counter2 = 0; 
+app.get('/counter2', function (req, res) {
+    counter2+=1;
+    res.send(counter2.toString());
+    });
+
+ var counter3 = 0; 
+app.get('/counter3', function (req, res) {
+    counter3+=1;
+    res.send(counter3.toString());
+    });
+
 	
 	//http://ranjithdss15.imad.hasura-app.io/submit-comment?comment=comment3
   var comments=[];
+  var comments2=[];
+var comments3=[];
 app.get('/submit-comment/', function (req, res) {
     var comment= req.query.commentinput;
     comments.push(comment);
     res.send(JSON.stringify(comments));
+
+  });
+      app.get('/submit-comment2/', function (req, res) {
+        var comment2= req.query.commentinput2;
+    comments2.push(comment2);
+    res.send(JSON.stringify(comments2));
+  });
+      app.get('/submit-comment3/', function (req, res) {
+      var comment3= req.query.commentinput3;
+    comments3.push(comment3);
+    res.send(JSON.stringify(comments3));
     });
 
   
