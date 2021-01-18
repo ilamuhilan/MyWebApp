edb73d585ae3445183072ed21bf12fe64d0b0a29
Ranjith <30803859+Ranjithdss15@users.noreply.github.com>
2021-01-18 13:25:28 +0530

    Update Jenkinsfile.groovy

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -1,1 +1,29 @@
-
+ pipeline {
+	agent any
+	stages {
+		stage('GIT'){
+			steps{
+			    println "Checking out"
+			}
+		}
+		
+		stage('Unit Test'){
+		steps{
+		    println "Running Tests"
+			}
+		}
+		stage('Build'){
+		steps{
+            println "Building proj"
+			}
+		}
+		
+		stage('Deploy'){
+		steps{
+		    
+		    println "Deploying it to ${params.serverType} server"
+			}
+		}		
+	}
+ }
+ 
